5e301f2c9f5db03c1357e04a814ee8769626df11
Rian Fowler <4998130+rianfowler@users.noreply.github.com>
2025-05-11 11:13:43 -0500

    refactor: update MainLayout to use SidebarLayout for proper mobile support

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,17 +1,15 @@
 import { Outlet } from 'react-router-dom'
-import { Sidebar } from '@/components/sidebar'
+import { DefaultSidebar } from '@/components/sidebar'
 import { UserNavbar } from '@/components/navbar'
+import { SidebarLayout } from '@/components/sidebar-layout'
 
 export default function MainLayout() {
   return (
-    <div className="flex h-screen">
-      <Sidebar />
-      <div className="flex flex-1 flex-col">
-        <UserNavbar />
-        <main className="flex-1 overflow-y-auto bg-white px-4 py-8 dark:bg-zinc-900 sm:px-6 lg:px-8">
-          <Outlet />
-        </main>
-      </div>
-    </div>
+    <SidebarLayout
+      navbar={<UserNavbar />}
+      sidebar={<DefaultSidebar />}
+    >
+      <Outlet />
+    </SidebarLayout>
   )
 }
