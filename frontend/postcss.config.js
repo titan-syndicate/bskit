bd57fd896bacbb59193e72049eca9be8f4bd8dfc
Rian Fowler <4998130+rianfowler@users.noreply.github.com>
2025-05-10 19:35:27 -0500

    chore: migrate configuration files from old project

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,9 +1,9 @@
-import tailwindcss from '@tailwindcss/postcss7-compat'
-import autoprefixer from 'autoprefixer'
-
-export default {
-  plugins: [
-    tailwindcss,
-    autoprefixer,
-  ],
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
+
+export default config
